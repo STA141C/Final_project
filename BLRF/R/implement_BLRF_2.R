0149a40562b96d8ac659a32e0ebb1726253116a6
yuqing-yang <46581451+yuqing-yang@users.noreply.github.com>
2020-03-13 17:12:00 -0700

    implement_par

---
+++ BLRF/R/implement_BLRF_2.R
@@ -0,0 +1,45 @@
+
+
+#' Drive file to run BLRF
+#'
+#' @param formula A formula expression.
+#' @param data data.frame. Original data.
+#' @param gamma numeric. User_defined sizing factor.
+#' @param b numeric. Size of subsamples.
+#' @param s numeric. Number of subsamples.
+#' @param r numeric. Number of trees.
+#' @param n_var numeric. Number of variables to subset to build one tree.
+#' @param core numeric. Number of core.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+implement_BLRF <- function(formula, data, gamma, b = NULL, s, r, n_var, core = 1){
+  n <- nrow(data)
+  x_var <- strsplit(as.character(formula[3]), split = "[ ]\\+[ ]")
+  data <- data[, x_var[[1]]]
+  Subs <- subsampling(data, gamma, b, s)
+  if(core = 1){
+    Trees <- purrr::map(Subs, ~tree_implement(formula, subsample = ., r, n, n_var))
+  }
+  else if(core > 0){
+
+    plan(multiprocess, workers = core)
+
+    Trees <- future_map(Subs,
+
+                        ~{
+
+                          ~tree_implement(formula, subsample = ., r, n, n_var)
+
+                         },
+                        .options = future_options(scheduling = FALSE)
+
+                       )
+
+
+  }
+  #misclass <- all_eval(Trees)
+  return(Trees)
+}
