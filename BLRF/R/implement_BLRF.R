9bcc38f1df4f03a0312a678c9441f47076530f37
Yujia Lu <qianshangzen@gmail.com>
2020-03-13 17:27:20 -0700

    updated ed

--- BLRF/R/implement_BLRF.R
+++ BLRF/R/implement_BLRF.R
@@ -18,9 +18,11 @@
 implement_BLRF <- function(formula, data, gamma, b = NULL, s, r, n_var, core = 1){
   n <- nrow(data)
   x_var <- strsplit(as.character(formula[3]), split = "[ ]\\+[ ]")
-  data <- data[, x_var[[1]]]
+  if(x_var != '.') {
+    data <- data[, x_var[[1]]]
+  }
   Subs <- subsampling(data, gamma, b, s)
-  if(core = 1){
+  if(core == 1){
     Trees <- purrr::map(Subs, ~tree_implement(formula, subsample = ., r, n, n_var))
   }
   else if(core > 0){
