cfc2b0d622b5a08c712218fa1f2b5072a979b9eb
Yating Ge <yatingge1225@gmail.com>
2020-03-15 16:33:23 -0700

    add tests for tiny regression data

--- BLRF/tests/testthat/test_accuracy_mean_ci.R
+++ BLRF/tests/testthat/test_accuracy_mean_ci.R
@@ -2,10 +2,10 @@ context("Confidence interval for mean classification accuracy rate")
 
 test_that("CI for mean classification accuracy rate",{
 
-  load("train_sample.Rda")
-  load('test_sample.Rda')
-  trees <- tree_implement(Type~., train_sample, r = 10, n=200, n_var= 5, split = "gini")
-  result <- accuracy_mean_ci(trees, test_sample, lower = 0.025, upper = 0.975)
+  load("../../tinydata/train_glass_sample.Rda")
+  load("../../tinydata/test_glass_sample.Rda")
+  trees <- tree_implement(Type~., train_glass_sample, r = 10, n=200, n_var= 5, split = "gini")
+  result <- accuracy_mean_ci(trees, test_glass_sample, lower = 0.025, upper = 0.975)
   expect_equal(class(result), "matrix")
   expect_equal(typeof(result), 'double')
 })
