7caf05f93f0f6bebf56639db67892fa72adc3ee1
Yating Ge <yatingge1225@gmail.com>
2020-03-19 11:40:03 -0700

    delete multiple prediction test

--- BLRF/tests/testthat/test_prediction_tree_regression.R
+++
@@ -1,9 +0,0 @@
-context("Predictions on BLTF regression problems")
-
-test_that('predictions on BLTF regression problems',{
-  load("../../tinydata/train_mortality_sample.Rda")
-  load("../../tinydata/test_mortality_sample.Rda")
-  trees <- tree_implement(MORTALITY~., train_mortality_sample, r = 10, n=70, n_var= 5, split = "gini")
-  result <- prediction_tree_regression(trees, test_mortality_sample)
-  expect_equal(class(result), 'numeric')
-})
